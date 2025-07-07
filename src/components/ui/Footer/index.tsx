ea32ab043b633f76338187aa9d5e9dcc18aaa0f2
samuel <samuellima.rocha@hotmail.com>
2025-07-06 22:37:28 -0300

    Ajuste

--- src/components/ui/Footer/index.tsx
+++ src/components/ui/Footer/index.tsx
@@ -3,7 +3,7 @@ import styles from "./styles.module.scss";
 export function Footer() {
   return (
     <footer className={styles.footerContainer}>
-      <h1>Copyright © 2023 - Express Waiter | Todos os direitos reservados </h1>
+      <h1>Copyright © 2025 - Express Waiter | Todos os direitos reservados </h1>
     </footer>
   );
 }
